ae132956385249a88265b007ed93331b59cc745b
Istvan Acs <istvan.acs89@gmail.com>
2020-02-01 19:11:23 +0000

    Add environment variables

--- server/src/index.js
+++ server/src/index.js
@@ -2,15 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const middlewares = require('./middlewares');
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+});
+
 app.use(morgan('common'));
 app.use(helmet());
 app.use(
   cors({
-    origin: ' http://localhost:3000'
+    origin: process.env.CORS_ORIGIN
   })
 );
 
