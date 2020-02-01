2cd5c29bd00ada0940e901b3423357293bec6e7a
Istvan Acs <istvan.acs89@gmail.com>
2020-02-01 14:15:44 +0000

    Add middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: ' http://localhost:3000'
+  })
+);
 
 const port = process.env.PORT || 1337;
 
